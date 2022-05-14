c9437e9d0e229f066e45814134dbd0e68b1b433e
Nicolas Orozco Davila <nico.orozco.davila@yandex.com>
2022-05-13 19:42:31 -0500

    Add rounded to image wrapper

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,24 +1,24 @@
 function Main() {
   return (
     <main className="flex flex-col md:flex-row text-center md:text-left text-white font-inter font-extrabold text-lg xl:text-xl gap-6 md:gap-14 lg:gap-20">
-      <span>
+      <p>
         10k+{" "}
-        <p className="text-semiwhite-200 font-normal uppercase text-xs mt-1 tracking-wider">
+        <span className="text-semiwhite-200 font-normal uppercase text-xs mt-1 tracking-wider">
           companies
-        </p>
-      </span>
-      <span>
+        </span>
+      </p>
+      <p>
         314{" "}
-        <p className="text-semiwhite-200 font-normal uppercase text-xs mt-1 tracking-wider">
+        <span className="text-semiwhite-200 font-normal uppercase text-xs mt-1 tracking-wider">
           Templates
-        </p>
-      </span>
-      <span>
+        </span>
+      </p>
+      <p>
         12M+{" "}
-        <p className="text-semiwhite-200 font-normal uppercase text-xs mt-1 tracking-wider">
+        <span className="text-semiwhite-200 font-normal uppercase text-xs mt-1 tracking-wider">
           Queries
-        </p>
-      </span>
+        </span>
+      </p>
     </main>
   );
 }
