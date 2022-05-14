464216379037c04a873b52b936299186aa997ca9
Nicolas Orozco Davila <nico.orozco.davila@yandex.com>
2022-05-13 19:47:27 -0500

    Fix img attributes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-
+import IMG from "../assets/image-header-desktop.jpg";
 function Header() {
   return (
     <header className="w-full md:w-1/2 bg-violet rounded-t-md md:rounded-tr-md md:rounded-br-md">
       <img
         role="presentation"
+        src={IMG}
+        alt=""
         className="w-full h-full opacity-75 mix-blend-multiply object-cover object-left-top bg-pink content-[url('/src/assets/image-header-mobile.jpg')] md:content-[url('/src/assets/image-header-desktop.jpg')]"
       />
     </header>
